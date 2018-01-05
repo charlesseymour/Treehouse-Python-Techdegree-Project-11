949958f24280575c05ee0c8aa89c2cb7dc1c8ca0
Charles Seymour <charlesseymour@yahoo.com>
2018-01-05 14:44:09 -0600

    Created models and migrated

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -1,4 +1,97 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
+class Dog(models.Model):
+    name = models.CharField(max_length=20)
+    image_filename = models.CharField(max_length=200)
+    breed = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    MALE = 'm'
+    FEMALE = 'f'
+    UNKNOWN = 'u'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (UNKNOWN, 'Unknown'),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default=UNKNOWN
+    )
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    EXTRA_LARGE = 'xl'
+    SIZE_CHOICES = (
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (EXTRA_LARGE, 'Extra Large'),
+        (UNKNOWN, 'Unknown'),
+    )
+    size = models.CharField(
+        max_length=1,
+        choices=SIZE_CHOICES,
+        default=UNKNOWN
+    )
+    
+    
+class UserDog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    LIKED = 'l'
+    DISLIKED = 'd'
+    STATUS_CHOICES = (
+        (LIKED, 'Like'),
+        (DISLIKED, 'Dislike'),
+    )
+    status = models.CharField(
+        max_length=1, 
+        choices=STATUS_CHOICES,
+        default=LIKED
+    )
+    
+class UserPref(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    BABY = 'b'
+    YOUNG = 'y'
+    ADULT = 'a'
+    SENIOR = 's'
+    AGE_CHOICES = (
+        (BABY, 'Baby'),
+        (YOUNG, 'Young'),
+        (ADULT, 'Adult'),
+        (SENIOR, 'Senior'),
+    )
+    age = models.CharField(
+        max_length=1,
+        choices=AGE_CHOICES,
+        default=BABY
+    )
+    MALE = 'm'
+    FEMALE = 'f'
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default=MALE
+    )
+    SMALL = 's'
+    MEDIUM = 'm'
+    LARGE = 'l'
+    EXTRA_LARGE = 'xl'
+    SIZE_CHOICES = (
+        (SMALL, 'Small'),
+        (MEDIUM, 'Medium'),
+        (LARGE, 'Large'),
+        (EXTRA_LARGE, 'Extra large'),
+    )
+    size = models.CharField(
+        max_length=1,
+        choices=SIZE_CHOICES,
+        default=SMALL
+    )
