b9ea94f05a3eea7efc548f1a68b8b708149f6bfe
Charles Seymour <charlesseymour@yahoo.com>
2018-01-13 18:23:49 -0600

    Updated put method in RetrieveUpdateUserPref to allow for creating new userpref

--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 from rest_framework import permissions, status
 from rest_framework.views import APIView
@@ -25,19 +26,25 @@ class RetrieveUpdateUserPref(RetrieveUpdateAPIView):
         )
         
     def put(self, request, format=None):
-        user_pref = self.get_object()
         data = request.data
         data['age'] = data['age'].split(',')
         data['gender'] = data['gender'].split(',')
         data['size'] = data['size'].split(',')
-        serializer = serializers.UserPrefSerializer(user_pref,
-                                                    data=request.data)
-        print(serializer.is_valid())
+        try:
+            user_pref = self.get_object()
+            serializer = serializers.UserPrefSerializer(user_pref,
+                                                        data=data)
+            if serializer.is_valid():
+                serializer.save(user=self.request.user)
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+            serializer = serializers.UserPrefSerializer(data=data)
         if serializer.is_valid():
             serializer.save(user=self.request.user)
             return Response(serializer.data)
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
     
 # class ListUndecidedDog(APIView):
     # def get(self, request, format=None):
