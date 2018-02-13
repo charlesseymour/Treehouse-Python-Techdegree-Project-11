4d3cace12fd7a3e5d01aa9dce74afe2cfc2f7fb6
Charles Seymour <charlesseymour@yahoo.com>
2018-02-12 21:20:47 -0600

    Added tests for retrieving user preferences

--- backend/pugorugh/tests.py
+++ backend/pugorugh/tests.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django_webtest import WebTest
 from rest_framework import status
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIClient
 
 from .models import Dog, UserPref, UserDog
 
@@ -51,14 +52,62 @@ class MenuTests(WebTest):
         self.assertEqual(user_dog.status, 'l')
         
 class ViewTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('temporary', 'temp@example.com',
+                                 'temporary')
+        self.user.save()
+        token = Token.objects.create(user=self.user)
+        token.save()
+        self.user_pref = UserPref.objects.create(age='b,y',
+                                            gender='m,f',
+                                            size='s,m',
+                                            user=self.user)
+        self.user_pref.save()
+        
     def test_user_register(self):
         url = reverse('register-user')
-        data = {'username': 'temporary', 'password': 'password'}
+        data = {'username': 'test_user', 'password': 'password'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, 'temporary')
-        self.assertTrue(User.objects.get().check_password('password'))
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(id=2).username, 'test_user')
+        self.assertTrue(User.objects.get(id=2).check_password('password'))
+        
+    def test_retrieve_user_pref_with_token(self):
+        url = reverse('userpref-detail')
+        token = Token.objects.get(user__username='temporary')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['age']), 2)
+        self.assertIn('b', response.data['age'])
+        self.assertIn('y', response.data['age'])
+        self.assertEqual(len(response.data['gender']), 2)
+        self.assertIn('m', response.data['gender'])
+        self.assertIn('f', response.data['gender'])
+        self.assertEqual(len(response.data['size']), 2)
+        self.assertIn('s', response.data['size'])
+        self.assertIn('m', response.data['size'])
+        
+    def test_retrieve_user_pref_without_token(self):
+        url = reverse('userpref-detail')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+    def test_update_user_pref_with_token(self):
+        url = reverse('userpref-detail')
+        data = {'user': self.user.id, 'age': 'b', 'gender': 'm', 'size': 's,m,l'}
+        token = Token.objects.get(user__username='temporary')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = self.client.put(url, data, format='json')
+        print(response)
+        
+        
+        
+        
+        
+        
+        
         
         
         
